ede9a97b1b1162dbe587690490527ffc44d9ecd7
Saurab Prasai <saurablovetocode@gmail.com>
2023-09-06 19:37:48 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const btn=document.getElementById('btn');
+const apiKey="6f533cdba10e64d9836782b2d22dda51"
+const temperature=document.querySelector('.tempt')
+const descrip=document.querySelector('.first')
+const name=document.querySelector('.last')
+
+
+const getWeather=async()=>{ 
+const city=document.getElementById('city');
+const cityName=city.value; 
+
+//fetching the data
+const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+const data=await response.json()
+temperature.innerHTML=`${data.main.temp}&#176C`
+descrip.innerHTML=data.weather[0].description.toUpperCase();
+name.innerHTML=`${data.name.toUpperCase()} ${data.coord.lon},${data.coord.lat}`
+ city.value=""
+}
+
+btn.addEventListener('click',getWeather)
+
